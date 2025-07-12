bda06d4981acaa8df122e0e9cd720daacb666bb7
kbrunot . <eukevinbruno@gmail.com>
2025-07-12 10:34:18 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// mobile/App.js
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './src/navigation/AppNavigator';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+  );
+}
